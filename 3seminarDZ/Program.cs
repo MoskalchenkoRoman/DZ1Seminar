0e5599b5bc70788f24ac12d50eaf49ae12717c61
roman <demos6988@mail.ru>
2022-08-26 20:19:33 +0300

    DZ_3siminarr

---
+++ 3seminarDZ/Program.cs
@@ -0,0 +1,36 @@
+﻿// DZ_3
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// double kub = Math.Pow(num,3);
+// Console.Write(num+" - в кубе = "+kub);
+
+// DZ_2
+// Console.WriteLine("Введите xa");
+// int xa = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите ya");
+// int ya = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите za");
+// int za = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите xb");
+// int xb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите yb");
+// int yb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите zb");
+// int zb = Convert.ToInt32(Console.ReadLine());
+
+// double ras = Math.Sqrt(Math.Pow(xb-xa,2) + Math.Pow(yb-ya,2) + Math.Pow(zb-za,2));
+
+// Console.Write("A("+xa+","+ya+","+za+")"+" "+"B("+xb+","+yb+","+zb+")"+"="+ras);
+
+
+// DZ_1
+// Console.WriteLine("Введите 5-значное число");
+// string num = Console.ReadLine();
+// if(num[0]==num[4] && num[1]==num[3])
+// {
+//     Console.Write(num+" -является палиндромом");
+// }
+// else
+// {
+//     Console.Write(num+" -число не палиндром");
+// }
